955b0ce73314dfe398509c689cffb3ad452283e7
Ilya Belik <141572840+Ateglif@users.noreply.github.com>
2023-11-13 02:08:51 +0500

    Update UserDAOImpl.java

--- src/main/java/spring/DAO/UserDAOImpl.java
+++ src/main/java/spring/DAO/UserDAOImpl.java
@@ -1,16 +1,12 @@
 package spring.DAO;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import spring.Model.User;
-
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository
 public class UserDAOImpl implements UserDAO {
-    @Autowired
     @PersistenceContext
     private EntityManager entityManager;
 
@@ -24,7 +20,7 @@ public class UserDAOImpl implements UserDAO {
         if (user.getId() == 0) {
             entityManager.persist(user);
         } else {
-            entityManager.merge(user);
+            updateUser(user);
         }
     }
 
@@ -40,5 +36,9 @@ public class UserDAOImpl implements UserDAO {
         if(user!= null){
             entityManager.remove(user);
         }
+    }
+    @Override
+    public void updateUser(User user) {
+        entityManager.merge(user);
     }
 }
